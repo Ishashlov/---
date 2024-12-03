2f8860f68c94d5163994972787ddd5e1ea678dd4
ivanS <ivanshashlov64@gmail.com>
2024-12-03 18:07:57 +0400

    version3

--- program.py
+++ program.py
@@ -1,7 +1,9 @@
+#https://github.com/Ishashlov/git_project1
+
 def main():
     print('My first git program')
-    print('And I change it every day, im autistic bruh')
-    print('hgdvskvck')
+    print('And I change it every day')
+    print('auugh')
 
 
 if __name__ == '__main__':
